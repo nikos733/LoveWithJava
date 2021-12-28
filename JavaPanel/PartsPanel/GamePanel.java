2b3c13d7e1f7d89efc9a81a34e12f2e561977afd
Nikita <ivin-nikita@ro.ru>
2021-12-28 23:27:45 +0300

    HomeWork 7 PART III (GamePanel)

---
+++ JavaPanel/PartsPanel/GamePanel.java
@@ -0,0 +1,10 @@
+package JavaPanel.PartsPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+    public GamePanel() {
+        setBackground(Color.GRAY);
+    }
+}
